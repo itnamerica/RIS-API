c233737df1dd633bdda5b6c92d00679b0113e2b6
Dragos Stancu <dragos@webfixstudio.com>
2023-09-18 22:23:27 -0400

    css

--- src/templates/templates-widget.mjs
+++ src/templates/templates-widget.mjs
@@ -6,8 +6,15 @@ const CSS_DEFAULT = `
 
 const CSS_RIS = `
 <style>
+.ris-widget{
+    padding:2em 0;
+}
 .ris-widget *{
     box-sizing: border-box;
+}
+
+.ris-widget h3{
+    font-size:20px;
 }
 
 .ris-widget button,
@@ -100,85 +107,67 @@ ${SCRIPT_DEFAULT}
 `;
 
 const HTML_WIDGET = `<form method="get" id="ris-search-program">
-<h2 class="center-text pt-3 pb-2">Search for transportation programs</h2>
-<ul class="columns align-c">
-    <li class="pr-3 w-25 m-w-100">
-        <div class="block pb-2">
-            <select class="w-100" id="ris-state">
-                <option value="">... select state</option>
-                <option value="AL">Alabama</option>
-                <option value="AK">Alaska</option>
-                <option value="AZ">Arizona</option>
-                <option value="AR">Arkansas</option>
-                <option value="CA">California</option>
-                <option value="CO">Colorado</option>
-                <option value="CT">Connecticut</option>
-                <option value="DE">Delaware</option>
-                <option value="DC">District Of Columbia</option>
-                <option value="FL">Florida</option>
-                <option value="GA">Georgia</option>
-                <option value="HI">Hawaii</option>
-                <option value="ID">Idaho</option>
-                <option value="IL">Illinois</option>
-                <option value="IN">Indiana</option>
-                <option value="IA">Iowa</option>
-                <option value="KS">Kansas</option>
-                <option value="KY">Kentucky</option>
-                <option value="LA">Louisiana</option>
-                <option value="ME">Maine</option>
-                <option value="MD">Maryland</option>
-                <option value="MA">Massachusetts</option>
-                <option value="MI">Michigan</option>
-                <option value="MN">Minnesota</option>
-                <option value="MS">Mississippi</option>
-                <option value="MO">Missouri</option>
-                <option value="MT">Montana</option>
-                <option value="NE">Nebraska</option>
-                <option value="NV">Nevada</option>
-                <option value="NH">New Hampshire</option>
-                <option value="NJ">New Jersey</option>
-                <option value="NM">New Mexico</option>
-                <option value="NY">New York</option>
-                <option value="NC">North Carolina</option>
-                <option value="ND">North Dakota</option>
-                <option value="OH">Ohio</option>
-                <option value="OK">Oklahoma</option>
-                <option value="OR">Oregon</option>
-                <option value="PA">Pennsylvania</option>
-                <option value="RI">Rhode Island</option>
-                <option value="SC">South Carolina</option>
-                <option value="SD">South Dakota</option>
-                <option value="TN">Tennessee</option>
-                <option value="TX">Texas</option>
-                <option value="UT">Utah</option>
-                <option value="VT">Vermont</option>
-                <option value="VA">Virginia</option>
-                <option value="WA">Washington</option>
-                <option value="WV">West Virginia</option>
-                <option value="WI">Wisconsin</option>
-                <option value="WY">Wyoming</option>
-            </select>
-        </div>
-    </li>
-    <li class="pr-3 w-25 m-w-100 disabled ris-section-actions">
-        <div class="block pb-2">
-            <select class="w-100" id="ris-county">
-                <option value="">... select county</option>
-            </select>
-        </div>
-    </li>
-    <!--<li class="pr-3 w-25 m-w-100">
-        <div class="block pb-2">
-            <input type="text" id="keyword" name="ris_keyword" placeholder="keyword" class="w-100" value="">
-            <h6 class="p-0 m-0">i.e. Wheelchair, Free</h6>
-        </div>
-    </li>-->
-    <li class="pr-3 w-25 m-w-100 disabled ris-section-actions">
-        <div class="block">
-            <input type="button" value="Search" id="ris-list-programs" class="w-100 button-search">
-        </div>
-    </li>
-</ul>
+<h3>Search for transportation programs</h3>
+<div class="flex">
+    <select id="ris-state">
+        <option value="">... select state</option>
+        <option value="AL">Alabama</option>
+        <option value="AK">Alaska</option>
+        <option value="AZ">Arizona</option>
+        <option value="AR">Arkansas</option>
+        <option value="CA">California</option>
+        <option value="CO">Colorado</option>
+        <option value="CT">Connecticut</option>
+        <option value="DE">Delaware</option>
+        <option value="DC">District Of Columbia</option>
+        <option value="FL">Florida</option>
+        <option value="GA">Georgia</option>
+        <option value="HI">Hawaii</option>
+        <option value="ID">Idaho</option>
+        <option value="IL">Illinois</option>
+        <option value="IN">Indiana</option>
+        <option value="IA">Iowa</option>
+        <option value="KS">Kansas</option>
+        <option value="KY">Kentucky</option>
+        <option value="LA">Louisiana</option>
+        <option value="ME">Maine</option>
+        <option value="MD">Maryland</option>
+        <option value="MA">Massachusetts</option>
+        <option value="MI">Michigan</option>
+        <option value="MN">Minnesota</option>
+        <option value="MS">Mississippi</option>
+        <option value="MO">Missouri</option>
+        <option value="MT">Montana</option>
+        <option value="NE">Nebraska</option>
+        <option value="NV">Nevada</option>
+        <option value="NH">New Hampshire</option>
+        <option value="NJ">New Jersey</option>
+        <option value="NM">New Mexico</option>
+        <option value="NY">New York</option>
+        <option value="NC">North Carolina</option>
+        <option value="ND">North Dakota</option>
+        <option value="OH">Ohio</option>
+        <option value="OK">Oklahoma</option>
+        <option value="OR">Oregon</option>
+        <option value="PA">Pennsylvania</option>
+        <option value="RI">Rhode Island</option>
+        <option value="SC">South Carolina</option>
+        <option value="SD">South Dakota</option>
+        <option value="TN">Tennessee</option>
+        <option value="TX">Texas</option>
+        <option value="UT">Utah</option>
+        <option value="VT">Vermont</option>
+        <option value="VA">Virginia</option>
+        <option value="WA">Washington</option>
+        <option value="WV">West Virginia</option>
+        <option value="WI">Wisconsin</option>
+        <option value="WY">Wyoming</option>
+    </select>
+    <select id="ris-county">
+        <option value="">... select county</option>
+    </select>
+    <button id="ris-list-programs">Search</button>
+</div>
 </form>
 <div id="ris-progress"></div>
 <div id="ris-results"></div>`;
